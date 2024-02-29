b3e4f0dae64c48926c210f5bb2826f4a83eec500
Tue Anh <122685597+ntalalala@users.noreply.github.com>
2024-02-29 16:20:38 +0700

    Create dlemur-laptop-mobile-viewership.sql

---
+++ Practice4/dlemur-laptop-mobile-viewership.sql
@@ -0,0 +1,30 @@
+/*
+https://datalemur.com/questions/laptop-mobile-viewership
+
+Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership. 
+Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views.
+
+viewership Table:
+
++-------------+--------------+
+| Column Name | Type         |
++-------------+--------------+
+| user_id     | integer      |
+| device_type | string       |
+| view_time   | timestamp    |
++-------------+--------------+
+*/
+SELECT 
+SUM(
+  CASE 
+    WHEN device_type = 'laptop' THEN 1
+    ELSE 0
+  END
+) AS laptop_views,
+SUM(
+  CASE
+    WHEN device_type != 'laptop' THEN 1
+    ELSE 0
+  END
+) AS mobile_views
+FROM viewership;
