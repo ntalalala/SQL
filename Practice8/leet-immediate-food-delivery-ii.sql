d6461af7b09b85878d056038ec00cb6579ccc1cd
Tue Anh <122685597+ntalalala@users.noreply.github.com>
2024-03-08 23:57:25 +0700

    Create leet-immediate-food-delivery-ii.sql

---
+++ Practice8/leet-immediate-food-delivery-ii.sql
@@ -0,0 +1,59 @@
+/*
+https://leetcode.com/problems/immediate-food-delivery-ii/description/?envType=study-plan-v2&envId=top-sql-50
+
+If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+
+The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
+Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+
+Table: Delivery
+
++-----------------------------+---------+
+| Column Name                 | Type    |
++-----------------------------+---------+
+| delivery_id                 | int     |
+| customer_id                 | int     |
+| order_date                  | date    |
+| customer_pref_delivery_date | date    |
++-----------------------------+---------+
+delivery_id is the column of unique values of this table.
+The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+
+Input: 
+Delivery table:
++-------------+-------------+------------+-----------------------------+
+| delivery_id | customer_id | order_date | customer_pref_delivery_date |
++-------------+-------------+------------+-----------------------------+
+| 1           | 1           | 2019-08-01 | 2019-08-02                  |
+| 2           | 2           | 2019-08-02 | 2019-08-02                  |
+| 3           | 1           | 2019-08-11 | 2019-08-12                  |
+| 4           | 3           | 2019-08-24 | 2019-08-24                  |
+| 5           | 3           | 2019-08-21 | 2019-08-22                  |
+| 6           | 2           | 2019-08-11 | 2019-08-13                  |
+| 7           | 4           | 2019-08-09 | 2019-08-09                  |
++-------------+-------------+------------+-----------------------------+
+Output: 
++----------------------+
+| immediate_percentage |
++----------------------+
+| 50.00                |
++----------------------+
+Explanation: 
+The customer id 2, 4 have a first order with delivery id 2 and it is immediate in 4 customers.
+
+*/
+WITH cte AS (
+    SELECT * 
+    FROM (
+        SELECT *,
+        RANK() OVER(PARTITION BY customer_id ORDER BY order_date) AS rank
+        FROM Delivery
+    )
+    WHERE rank = 1
+)
+
+SELECT ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM cte), 2) AS immediate_percentage
+FROM cte
+WHERE order_date = customer_pref_delivery_date;
+
